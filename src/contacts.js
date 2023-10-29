a6d395d633f9b428234a7b5e4bc2f1b2d590a45c
Marygopu143 <gopu06@email.franklin.edu>
2023-10-30 00:41:06 +0300

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,39 @@
+const contacts = [
+    {
+      name: "James",
+      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+      phone: "+1 (501) 432-0245",
+      email: "james.aydin@franklin.edu", 
+      linkedIn:"https://www.linkedin.com/in/james-a383921b9/"
+    },
+    {
+      name: "Ken",
+      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+      phone: "+1 (501) 112-0344",
+      email: "ken.aydin@franklin.edu",
+      linkedIn:"https://www.linkedin.com/in/ken-a383921b9/"
+    },
+    {
+        name: "Kendal",
+        imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+        phone: "+1 (501) 552-0574",
+        email: "kendal.aydin@franklin.edu", 
+        linkedIn:"https://www.linkedin.com/in/kendal-a383921b9/"
+    },
+    {
+        name: "Gianna",
+        imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+        phone: "+1 (501) 232-0674",
+        email: "gianna.aydin@franklin.edu",
+        linkedIn:"https://www.linkedin.com/in/gianna-a383921b9/"
+    },
+    {
+      name: "Rose",
+      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+      phone: "+1 (501) 042-0774",
+      email: "rose.aydin@franklin.edu",
+      linkedIn:"https://www.linkedin.com/in/rose-a383921b9/"
+    }
+  ];
+  
+  export default contacts;
