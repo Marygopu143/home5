0f29a564d1f268344167de03f490604cdeb01dc2
Marygopu143 <gopu06@email.franklin.edu>
2023-12-08 11:23:42 +0300

    init

--- src/contacts.js
+++ src/contacts.js
@@ -1,39 +1,45 @@
 const contacts = [
-    {
-      name: "James",
-      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
-      phone: "+1 (501) 432-0245",
-      email: "james.aydin@franklin.edu", 
-      linkedIn:"https://www.linkedin.com/in/james-a383921b9/"
-    },
-    {
-      name: "Ken",
-      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
-      phone: "+1 (501) 112-0344",
-      email: "ken.aydin@franklin.edu",
-      linkedIn:"https://www.linkedin.com/in/ken-a383921b9/"
-    },
-    {
-        name: "Kendal",
-        imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
-        phone: "+1 (501) 552-0574",
-        email: "kendal.aydin@franklin.edu", 
-        linkedIn:"https://www.linkedin.com/in/kendal-a383921b9/"
-    },
-    {
-        name: "Gianna",
-        imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
-        phone: "+1 (501) 232-0674",
-        email: "gianna.aydin@franklin.edu",
-        linkedIn:"https://www.linkedin.com/in/gianna-a383921b9/"
-    },
-    {
-      name: "Rose",
-      imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
-      phone: "+1 (501) 042-0774",
-      email: "rose.aydin@franklin.edu",
-      linkedIn:"https://www.linkedin.com/in/rose-a383921b9/"
-    }
-  ];
-  
-  export default contacts;
+  {
+    name: "Kemal",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 442-0274",
+    email: "kemal.aydin@franklin.edu"
+  },
+  {
+    name: "John Smith",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 432-0245",
+    email: "john.smith@example.com",
+    linkedIn: "https://www.linkedin.com/in/john-smith/"
+  },
+  {
+    name: "Emma Johnson",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 112-0344",
+    email: "emma.johnson@example.com",
+    linkedIn: "https://www.linkedin.com/in/emma-johnson/"
+  },
+  {
+    name: "Alexander Brown",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 552-0574",
+    email: "alexander.brown@example.com",
+    linkedIn: "https://www.linkedin.com/in/alexander-brown/"
+  },
+  {
+    name: "Sophia Davis",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 232-0674",
+    email: "sophia.davis@example.com",
+    linkedIn: "https://www.linkedin.com/in/sophia-davis/"
+  },
+  {
+    name: "Michael Wilson",
+    imgURL: "https://pic.onlinewebfonts.com/thumbnails/icons_218090.svg",
+    phone: "+1 (501) 042-0774",
+    email: "michael.wilson@example.com",
+    linkedIn: "https://www.linkedin.com/in/michael-wilson/"
+  }
+];
+
+export default contacts;
